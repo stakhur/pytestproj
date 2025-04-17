2be659a3f76f19962d9a3166e574c6efff82f1b5
stakhur <pushthebo@gmail.com>
2025-04-17 07:09:08 +0200

    make function for the game

--- twt_01_quiz/quiz_game.py
+++ twt_01_quiz/quiz_game.py
@@ -6,23 +6,26 @@ core = [
     ("What does ROM mean? ", "read only memory", 1),
 ]
 
-print("Welcome to my computer quiz!")
+def start_quiz_game(model):
+    print("Welcome to my computer quiz!")
 
-# get user input about should we start new game
-answer = input("Do you want to start new game? ")
-if answer.lower() != "yes":
-    quit()
+    # get user input about should we start new game
+    answer = input("Do you want to start new game? ")
+    if answer.lower() != "yes":
+        quit()
 
-print("Let's play :)")
-score = 0
+    print("Let's play :)")
+    score = 0
 
-for (question, answer, points) in core:
-    user_answer = input(question).lower()
-    if user_answer == answer:
-        print("Correct")
-        score += points
-    else:
-        print("Incorrect")
+    for (question, answer, points) in model:
+        user_answer = input(question).lower()
+        if user_answer == answer:
+            print("Correct")
+            score += points
+        else:
+            print("Incorrect")
 
-print("You got {} question{} correct.".format(score, "" if score == 1 else "s"))
-print("You got {}%.".format((score / len(core)) * 100, "" if score == 1 else "s"))
+    print("You got {} question{} correct.".format(score, "" if score == 1 else "s"))
+    print("You got {}%.".format((score / len(core)) * 100, "" if score == 1 else "s"))
+
+start_quiz_game(core)
