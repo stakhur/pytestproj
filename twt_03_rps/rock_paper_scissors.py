d5a49035b89c779361dae6b8d77785f9e945143d
stakhur <pushthebo@gmail.com>
2025-04-18 08:42:53 +0200

    added sugar

--- twt_03_rps/rock_paper_scissors.py
+++ twt_03_rps/rock_paper_scissors.py
@@ -1,11 +1,12 @@
 import random
 
-types = ("Rock", "Paper", "Scissors")
-rules = {
-    "Rock": "Scissors",
-    "Paper": "Rock",
-    "Scissors": "Paper",
-}
+types = ("rock", "paper", "scissors")
+# rules = {
+#     "rock": "scissors",
+#     "paper": "rock",
+#     "scissors": "paper",
+# }
+rules = {types[i]: types[i-1] for i in range(len(types))}
 
 
 def game():
@@ -13,25 +14,15 @@ def game():
     computer_wins = 0
 
     while True:
-        user_input = input("Type Rock/Paper/Scissors or Q to quit: ")
-        
-        if user_input.lower() == 'q':
-            print("User score: {}. Computer score: {}".format(user_wins, computer_wins))
-            result = ""
-            if user_wins > computer_wins:
-                result = "User won!"
-            elif user_wins < computer_wins:
-                result = "Computer won!"
-            else:
-                result = "Draw."
-            print("Game results: {}".format(result))
+        user_input = input("Type Rock/Paper/Scissors or Q to quit: ").lower()
+        if user_input == 'q':
             break
-        elif user_input not in rules.keys():
+        elif user_input not in types:
             print("Please type any of [Rock, Paper, Scissors, Q].")
             continue
 
         computer_input = types[random.randrange(3)]
-        print("Computer choose {}".format(computer_input))
+        print("Computer choose: {}".format(computer_input))
         
         if rules[user_input] == computer_input:
             user_wins += 1
@@ -42,5 +33,16 @@ def game():
         else:
             print("Draw.")
         print()
+
+    # Show game results    
+    print("User score: {}. Computer score: {}".format(user_wins, computer_wins))
+    result = ""
+    if user_wins > computer_wins:
+        result = "User won!"
+    elif user_wins < computer_wins:
+        result = "Computer won!"
+    else:
+        result = "Draw."
+    print("Game results: {}".format(result))
 
 game()
