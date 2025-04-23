613aa71d414a36c447e522855c65e03c6ba339fb
stakhur <pushthebo@gmail.com>
2025-04-23 08:57:49 +0200

    All players can complete their turn. show results at the end of the game.

--- twt_06_pig/pig.py
+++ twt_06_pig/pig.py
@@ -7,54 +7,70 @@ def roll():
 
     return roll
 
-def get_players():
-    max_players = 5
+def get_players(max_players=5):
     num_of_players = ""
 
-    while not isinstance(num_of_players, int) or (num_of_players < 2 or num_of_players > max_players):
-        num_of_players = input(f"Type number of players (2 - {max_players}): ")
+    while not isinstance(num_of_players, int) or num_of_players < 2 or num_of_players > max_players:
+        num_of_players = input(f"Enter the number of players (2 - {max_players}): ")
         if num_of_players.isdigit():
             num_of_players = int(num_of_players)
     
     return num_of_players
 
-def game():
-    POINTS_TO_WIN = 50
+def game(points_to_win=50):
     players = get_players()
-    players_scores = [0 for i in range(players)]
+    players_scores = [0 for _ in range(players)]
 
     current_player_id = 0
-    while all(x < POINTS_TO_WIN for x in players_scores):
-        current_player_scores = 0
-        while True:
-            choose = input(f"Player {current_player_id + 1} turn.\n"
-                           f"You have {players_scores[current_player_id]} points total. "
-                           f"You can save {current_player_scores} points from this round.\n"
-                           "Do you want to roll (Y/n)? ").lower()
-            if choose not in ("n", "y"):
-                continue
-
-            if choose == "n":
-                players_scores[current_player_id] += current_player_scores
-                print(f"You saved {current_player_scores} points. "
-                      f"You have {players_scores[current_player_id]} points total.")
-                if players_scores[current_player_id] >= POINTS_TO_WIN:
-                    print(f"Player {current_player_id + 1} win!")
-                break
-            else:
+    while max(players_scores) < points_to_win:
+        for current_player_id in range(players):
+            current_player_scores = 0
+            while True:
+                choose = input(f"Player {current_player_id + 1} turn.\n"
+                            f"You have {players_scores[current_player_id]} points total. "
+                            f"You can save {current_player_scores} points from this round.\n"
+                            "Do you want to roll (Y/n)? ").lower()
+                if choose not in ("", "n", "y"):
+                    continue
+                elif choose == "n":
+                    break
+                
                 points = roll()
-                if points != 1:
-                    current_player_scores += points
-                    print(f"You earn {points} points!")
-                else:
+                if points == 1:
                     current_player_scores = 0
                     print("It is 1. You loose your points :(")
                     break
-        
-        current_player_id += 1
-        if current_player_id >= players:
-            current_player_id = 0
+                else:
+                    current_player_scores += points
+                    print(f"You earn {points} points!")
+
+            if current_player_scores:
+                players_scores[current_player_id] += current_player_scores
+                print(f"You saved {current_player_scores} points. "
+                        f"You have {players_scores[current_player_id]} points total.")
+            
+            print()
+            current_player_id += 1
+            if current_player_id >= players:
+                current_player_id = 0
     
-    print(players_scores)
+    max_score = max(players_scores)
+    
+    players_won = []
+    prev_player_id = -1
+    while True:
+        try:
+            prev_player_id = players_scores.index(max_score, prev_player_id + 1)
+            players_won.append(prev_player_id)
+        except ValueError:
+            break
+
+    for player_won in players_won:
+        print(f"Player {player_won + 1} won!")
+
+    score_table_sorted = sorted(players_scores, reverse=True)
+    for score in score_table_sorted:
+        player_id = players_scores.index(score) + 1
+        print(f"{score} points - player {player_id}")
 
 game()
